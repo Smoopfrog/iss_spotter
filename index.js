403c2b671abd6c42955daa411549b68d45f89718
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-07-08 02:20:31 +0000

    added function printPassTimes

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const {nextISSTimesForMyLocation} = require('./iss');
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-
-//   console.log('It worked! Returned IP:' , ip);
-// });
-
-fetchCoordsByIP('50.92.37.19', (error, data) => {
-  if (error) {
-    console.log('Couldn\'t get coordinates.', error);
-  } else {
-    console.log('Your coordinates are:', data);
+const printPassTimes = (passTimes) => {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
 });
