d19bb815f9f9bf751e1bc9e20a244f7b1be14ca9
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-07-08 01:57:10 +0000

    added fetchISSFlyOverTimes function

--- iss.js
+++ iss.js
@@ -43,13 +43,41 @@ const fetchCoordsByIP = (ip, callback) => {
       callback(Error(msg), null);
       return;
     }
+    
+    const parsedBody = JSON.parse(body);
+    // check if "success" is true or not
+    if (!parsedBody.success) {
+      const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+      callback(Error(message), null);
+      return;
+    }
+
     const {latitude, longitude} = JSON.parse(body);
     callback(null, {latitude, longitude});
     return;
   });
 };
 
-module.exports = { 
+const fetchISSFlyOverTimes = (coordinates, callback) => {
+  const requestedURL = `https://iss-pass.herokuapp.com/json/?lat=${coordinates.latitude}&lon=${coordinates.longitude}`;
+
+  request(requestedURL, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const passes = JSON.parse(body).response;
+    callback(null, passes);
+  });
+};
+
+module.exports = {
   fetchMyIP,
-  fetchCoordsByIP
- };
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes
+};
