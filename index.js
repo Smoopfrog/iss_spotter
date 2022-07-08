13e25a64db88bc4138efa483b9a4064c9587bb9a
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-07-08 01:33:02 +0000

    added fetchCoordsByIP function to iss.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const { fetchMyIP } = require(',/iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+fetchCoordsByIP('50.92.37.19', (error, data) => {
+  if (error) {
+    console.log('Couldn\'t get coordinates.', error);
+  } else {
+    console.log('Your coordinates are:', data);
+  }
+});
--- iss.js
+++ iss.js
@@ -8,6 +8,7 @@ const request = require('request');
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
+
 const fetchMyIP = (callback) => {
   // use request to fetch IP address from JSON API
   const requestedURL = 'https://api.ipify.org/?format=json';
@@ -15,14 +16,40 @@ const fetchMyIP = (callback) => {
   request(requestedURL, (error, response, body) => {
     if (error) {
       callback(error, null);
-    } else {
-      const data = JSON.parse(body);
-      const ip = JSON.stringify(data.ip);
-      callback(null, ip);
+      return;
     }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
   });
 };
 
-fetchMyIP();
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = (ip, callback) => {
+  // use request to fetch IP coordinates address from JSON API
+  const requestedURL = `http://ipwho.is/${ip}`;
+
+  request(requestedURL, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP coordinates. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const {latitude, longitude} = JSON.parse(body);
+    callback(null, {latitude, longitude});
+    return;
+  });
+};
+
+module.exports = { 
+  fetchMyIP,
+  fetchCoordsByIP
+ };
